4b8376898298fd50cf1f1e3c7531f2eebde2122b
“Kes <kesia.development@gmail.com>
2021-03-26 16:36:15 +0100

    Add first template and jinja tags for datasets

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 
-from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for
+    )
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -16,14 +19,15 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+# Connect mongo to communicate with the flask app (instance created above)
 mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_data")
-def get_data():
-    data = mongo.db.data.find()
-    return render_template("data.html", data=data)
+@app.route("/get_datasets")
+def get_datasets():
+    datasets = mongo.db.datasets.find()
+    return render_template("datasets.html", datasets=datasets)
 
 
 if __name__ == "__main__":
